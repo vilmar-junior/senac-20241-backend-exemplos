2d17727eb5325894093d8ee9695fffb2480db7a8
Vilmar César Pereira Júnior <vilmarcesarpereira@gmail.com>
2024-06-12 20:10:07 -0300

    Teste

--- src/main/java/service/vemnox1/PartidaService.java
+++ src/main/java/service/vemnox1/PartidaService.java
@@ -3,7 +3,6 @@ package service.vemnox1;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import exception.VemNoX1Exception;
 import model.dto.vemnox1.JogadaDTO;
